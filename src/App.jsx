2a3dc4ecdafe340569e3140abd0c4fdea03f7428
Biozid Bhuiyan Tonoy <biozidbhuiyantonoy@gmail.com>
2026-05-13 19:16:23 +0600

    start the project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import MemoryCard from "./components/MemoryCard"
+
+
+function App() {
+  return (
+    <>
+     <MemoryCard/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MemoryCard.jsx
@@ -0,0 +1,15 @@
+
+
+function MemoryCard() {
+
+
+  return (
+    <>
+    <header>
+        <h1 className="text-3xl font-bold" >Memory Card</h1>
+    </header>
+    </>
+  )
+}
+
+export default MemoryCard
